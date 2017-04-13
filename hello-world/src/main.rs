32ee4b876b1fad5971294acaeccb995030a52faf
Alexander Ronald Altman <alexanderaltman@me.com>
2017-04-13 10:11:51 -0700

    Hello, world!

---
+++ hello-world/src/main.rs
@@ -0,0 +1,8 @@
+// This program prints "Hello, world!" to the screen.
+
+// This is a function named `main`.
+fn main() {
+    // This is a call to the macro `println` (the `!` is because it’s a macro).
+    // Its parameter is a string literal to be printed with a newline afterward.
+    println!("Hello, world!");
+}
